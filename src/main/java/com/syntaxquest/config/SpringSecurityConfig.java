29b35cdf54b83fbdb46340e4c83eb000aa666d76
tubolyjulia <tubolyjulia@gmail.com>
2025-01-22 17:29:29 +0100

    first steps project SyntaxQuest set up basics

--- src/main/java/com/syntaxquest/config/SpringSecurityConfig.java
+++ src/main/java/com/syntaxquest/config/SpringSecurityConfig.java
@@ -1,4 +1,9 @@
 package com.syntaxquest.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SpringSecurityConfig {
 }
--- src/main/java/com/syntaxquest/config/SpringWebConfig.java
+++ src/main/java/com/syntaxquest/config/SpringWebConfig.java
@@ -1,4 +1,9 @@
 package com.syntaxquest.config;
 
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Component
+@EnableWebMvc
 public class SpringWebConfig {
 }
