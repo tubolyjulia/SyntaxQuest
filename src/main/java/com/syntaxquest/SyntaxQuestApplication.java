459efd0b766ce2499f17163f9a98e5cb702432ae
tubolyjulia <tubolyjulia@gmail.com>
2025-01-22 17:12:02 +0100

    first steps project SyntaxQuest set up basics

---
+++ src/main/java/com/syntaxquest/SyntaxQuestApplication.java
@@ -0,0 +1,11 @@
+package com.syntaxquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyntaxQuestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SyntaxQuestApplication.class, args);
+    }
+}
---
+++ src/main/java/com/syntaxquest/config/SpringWebConfig.java
@@ -0,0 +1,4 @@
+package com.syntaxquest.config;
+
+public class SpringWebConfig {
+}
